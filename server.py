8155318a967f1fe0db763c537473a9ebc4d55981
Riizade <riizade@gmail.com>
2013-12-23 11:36:51 -0500

    Moved parsing functionality to a function in server.py

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import *
 import json
+import sqlalchemy
+from BeautifulSoup import BeautifulSoup
+import httplib2
 
 app = Flask(__name__)
 app.debug = True
@@ -11,6 +14,51 @@ def _():
 @app.route('/market/')
 def market():
     return render_template("market.html")
+
+#usage:
+#takes a starting page (set of 100 items)
+#returns the starting page to be used for the next call to ensure that there
+#are no duplicates and that the function doesn't ask for more items than exist
+def update_items(page_start):
+    page_count = 100
+
+    #get 100 items from Dota 2 Community Market
+    resp, content = httplib2.Http().request(
+        "http://steamcommunity.com/market/search/render/?query=appid%3A570&start=" 
+        + str(page_start) + "&count=" + str(page_count))
+
+    #DEBUG for working from file
+    #content = open('workfile.html', 'r').read()
+
+    request = json.loads(content)
+
+    content = request['results_html']
+    soup = BeautifulSoup(content)
+
+    #replace individual characters to clean up the HTML
+    temp_str = soup.prettify()
+    #remove \r, \n, \t, &gt, &lt, ;, \/span, \/div, \/a, }, &#36, USD, , (yes, the comma)
+    temp_str = temp_str.replace('\\r', '').replace('\\n', '').replace('\\t', '')
+    temp_str = temp_str.replace('&gt', '').replace('&lt', '').replace(';', '')
+    temp_str = temp_str.replace('\\/span', '').replace('\\/div', '').replace('\\/a', '')
+    temp_str = temp_str.replace('&#36', '').replace('}', '').replace('USD', '').replace(',', '')
+
+    soup = BeautifulSoup(temp_str)
+
+    for i in soup.findAll('a'):
+        quantity = i.div.div.span.span.contents[0].strip('\n\r ')
+        price = i.div.div.span.contents[6].strip('\n\r ')
+        name = i.div.contents[5].span.contents[0].strip('\n\r ')
+
+        #update item in database
+        #TODO
+
+    if (page_start + page_count >= int(request['total_count'])):
+        page_start = 0
+    else:
+        page_start += 100
+
+    return page_start
 
 
 if __name__ == '__main__':
