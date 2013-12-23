1c87e69fb6156e1b3baababb1820b9425bcbe7f7
Riizade <riizade@gmail.com>
2013-12-22 22:40:44 -0500

    Fixed Flask imports, defined market URL

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import flask
+from flask import *
 import json
 
 app = Flask(__name__)
@@ -7,3 +7,11 @@ app.debug = True
 @app.route('/')
 def _():
     return render_template("index.html")
+
+@app.route('/market/')
+def market():
+    return render_template("market.html")
+
+
+if __name__ == '__main__':
+    app.run()
