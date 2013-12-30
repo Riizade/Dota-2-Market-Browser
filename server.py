ba9a4ec612a5f14cd7e9e4339982260c9bf290d5
Riizade <riizade@gmail.com>
2013-12-30 01:56:05 -0500

    Added rudimentary item filtering

--- server.py
+++ server.py
@@ -11,6 +11,12 @@ app = Flask(__name__)
 # URL Routing
 #------------------------------------------------------------------------------
 
+def filter_results(results, attr, value):
+    if not value == None:
+        results = [i for i in results if getattr(i, attr) == value]
+    return results
+
+
 @app.route('/')
 def _():
     return render_template("index.html")
@@ -21,6 +27,19 @@ def market():
     logging.info('Market page accessed')
     results = session.query(MarketItem).all()
     session.close()
+
+    queries = [
+            'hero',
+            'item_slot',
+            'item_type',
+            'item_set',
+            'quality']
+
+    for query in queries:
+        results = filter_results(results, query, request.args.get(query))
+
+
+
     return render_template("market.html", items=results)
 
 #------------------------------------------------------------------------------
